bc8f0a5d11c3fb5d2ccb36ed12a017e6629c3d2b
jcmcarth06 <jcmcarth06@gmail.com>
2020-06-24 12:40:21 -0600

    add login and logout methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,54 @@
 class UsersController < ApplicationController
 
-    get '/users' do
+    get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :'users/show'
+    end
+  
+    get '/signup' do
+      if !logged_in?
+        erb :'users/signup', locals: {message: "Please sign up before you sign in"}
+      else
+        redirect to '/books'
+      end
+    end
+  
+    post '/signup' do
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect to '/signup'
+      else
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        @user.save
+        session[:user_id] = @user.id
+        redirect to '/books'
+      end
+    end
+  
+    get '/login' do
+      if !logged_in?
+        erb :'users/login'
+      else
+        redirect to '/books'
+      end
+    end
+  
+    post '/login' do
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect to "/books"
+      else
+        redirect to '/signup'
+      end
+    end
     
+    get '/logout' do
+      if logged_in?
+        session.destroy
+        redirect to '/login'
+      else
+        redirect to '/'
+      end
     end
-
-    get 'users/new' do
-
-    end
-
-    post '/users' do
-
-    end
-
-    get '/users/:id' do
-    
-    end
-
-    get '/users/:id/edit' do
-
-    end
-
-    put '/users/:id' do
-
-    end
-
-    delete '/users:id' do
-
-    end
-    
-end
+  
+  end
