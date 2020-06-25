58654550b5de715093cb94e5d6404cf3b27da568
jcmcarth06 <jcmcarth06@gmail.com>
2020-06-24 21:35:55 -0600

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20200624175350) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
